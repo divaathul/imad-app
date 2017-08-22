2f8bbc8bbea89cb899f36829c06504df4181e25d
divaathul <30874379+divaathul@users.noreply.github.com>
2017-08-22 21:08:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,21 @@
 //counter code.
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onClick = function() { 
 
+var request=XMLHttpRequest();
 
+request.onreadystatechange = function() {
+    if(request.readystate == XMLHttpRequest.DONE){
+        if(request.status == 20){
+            var counter = request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
 
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML = counter.toString();
+request.open('GET','http://imad.hasura.app.io/counter',true);
+request.send(null);
+
 };
